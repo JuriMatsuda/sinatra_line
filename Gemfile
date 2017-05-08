5b971782394e7efcca7dadffa13f168b0d372fb5
松田樹理 <matsudajuri@juri.local>
2017-05-08 20:03:05 +0900

    First commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+=begin
+gem "sinatra"
+gem "sinatra-contrib"
+gem "memcache-client"
+gem "foreman"
+gem "unicorn"
+gem "mysql2"
+gem "rack-flash3"
+gem "pry-byebug"
+=end
+
+gem "sinatra"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "sqlite3"
+gem "slim"
+gem "bcrypt"
+gem "rake"
---
+++ main.rb
@@ -0,0 +1,47 @@
+require 'sinatra'
+# require 'sinatra/reloader'
+require 'active_record'
+require 'bundler'
+Bundler.require
+
+
+ActiveRecord::Base.establish_connection(
+  "adapter"  => "sqlite3" ,
+  "database" => "./bbs.db"
+)
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
+class Comment < ActiveRecord::Base
+end
+
+get '/' do
+  @comments = Comment.order("id desc").all
+  erb :index
+end
+
+post '/new' do
+  Comment.create({:body => params[:body]})
+  redirect '/'
+end
+
+post '/delete' do
+  Comment.find(params[:id]).destroy
+end
+
+
+get '/singup' do
+　　
+end
+
+get '/login' do
+
+end
+
+get '/talks' do
+
+end
+
