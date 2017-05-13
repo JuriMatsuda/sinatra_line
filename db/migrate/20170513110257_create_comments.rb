f21363e2a61278b54e4db94a352c13c30aafbf0e
松田樹理 <matsudajuri@juri.local>
2017-05-13 20:42:52 +0900

    Commentsテーブル作成

---
+++ db/migrate/20170513110257_create_comments.rb
@@ -0,0 +1,9 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comments do |t|
+      t.integer :post_id, null: false
+      t.integer :user_id, null: false
+      t.text    :comment, null: false
+      t.timestamps
+  end
+end
--- models/user.rb
+++ models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :comments
   validates :name, presence: true
   validates :password, presence: true
   has_secure_password
